1dc0f020661f17c48826fd443196ac09f33acc6b
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-08-17 17:19:23 -0500

    data and pet images

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default [
+    {
+        id: 1,
+        name: 'Skeeter',
+        age: 9,
+        image: "example.img",
+    },
+    {
+        id: 2,
+        name: 'Bodie',
+        age: 15,
+        image: "example.img",
+    },
+    {
+        id: 3,
+        name: "Patti",
+        age: 2,
+        image: "example.img",
+    },
+    {
+        id: 4,
+        name: "Atlas",
+        age: 4,
+        image: "example.img",
+    },
+    {
+        id: 5,
+        name: "Oramge",
+        age: 5,
+        image: "example.img",
+    },
+    {
+        id: 6,
+        name: "Laverne",
+        age: 8,
+        image: "example.img",
+    },
+    {
+        id: 7,
+        name: "Persephone",
+        age: 4,
+        image: 'example.img',
+    },
+
+];
